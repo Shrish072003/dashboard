ca8fd0550d7b54de024fbd3f6fb88cee2359993b
rajanagh <132365124+rajanagh@users.noreply.github.com>
2023-08-13 16:19:37 +0530

    First commit
    This is first commit dated 13th of August

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const Users = require("./db/User");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.post("/register", async (req, res) => {
+  let user = new Users(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
+  res.send(result);
+});
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+  if (req.body.password && req.body.email) {
+    let user = await Users.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ result: "No user found" });
+    }
+  } else {
+    res.send({ result: "No user found" });
+  }
+});
+
+app.listen(5000);
